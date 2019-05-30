1300e51967f84bd16a8187e50498a52fcb01dfb9
Ethan Wang <ethanwang9011@gmail.com>
2019-05-30 18:35:08 +1200

    static host

---
+++ build/precache-manifest.651f9ff1d526a997844a2db27ccfaf6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54fbe62dad9c14c1d98bd8b07cbe0b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a1573ad058b8291414e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8accc5bbd7b38e6a76e4",
+    "url": "/static/js/2.7663dac8.chunk.js"
+  },
+  {
+    "revision": "3a1573ad058b8291414e",
+    "url": "/static/js/main.29b98685.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bbfff7e007b3195c84436d3f02630e40",
+    "url": "/static/media/model.bbfff7e0.png"
+  }
+]);
