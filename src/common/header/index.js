84dc1bb756bbebf93d4fb4e19c91b45919460ba7
Ethan Wang <ethanwang9011@gmail.com>
2019-05-30 15:48:18 +1200

    begin to edit home page

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import {
+    HeaderWrapper, 
+    CartWrapper,
+    CartInfo,
+} from './style';
 
 class Header extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            CartShow: false,
+        }
+    }
+
     render(){
         return(
-            <div>
-                This is header.
-            </div>
+            <HeaderWrapper>
+                <CartWrapper className={this.state.CartShow?"showCart":""}>
+                    <a onClick={()=>{this.CartShowHandler(this.state.CartShow)}}>My Cart</a>
+                    <CartInfo className={this.state.CartShow?"showCart":""}></CartInfo>
+                </CartWrapper>
+            </HeaderWrapper>
         )
     }
 
-    searchInfoShow = (isShow) => {
-        if (isShow){
-            return (
-                <div>
-                    extra content.
-                </div>
-            )
-        } 
+    CartShowHandler = (isShow) => {
+        this.setState({
+            CartShow: !isShow,
+        });
     }
 }
 
---
+++ src/common/header/style.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.div`
+    position: relative;
+    top: 15px;
+    height: 30px;
+    background-color: #f6f6f7;
+` 
+
+export const CartWrapper = styled.div`
+    text-align: center;
+    color: #9b9b9c;
+    line-height: 30px;
+    position: absolute;
+    height: 30px;
+    width: 80px;
+    left: 1208px;
+    font-size: 0.5em;
+    cursor: pointer;
+    &.showCart{
+        border: 1px solid #cccccc;
+        background-color: #ffffff;
+    }
+`
+
+export const CartInfo = styled.div`
+    position: absolute;
+    left: -254px;
+    top: 29px;
+    width: 293px;
+    height: 288px;
+    background-color: #ffffff;
+    border: 1px solid #cccccc;
+    padding: 0 20px;
+    display: none;
+    cursor: default;
+    &.showCart{
+        display: block;
+    }
+`
